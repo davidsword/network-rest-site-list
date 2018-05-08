5a272aafcd48739055edb68d563d94f592687584
David Sword <davidsword@Betsy.local>
2018-05-08 10:05:15 -0700

    🚀 inital commit

---
+++ network-rest.php
@@ -0,0 +1,59 @@
+<?php
+/*
+Plugin Name: Network REST Site List
+Plugin URI: https://github.com/davidsword/network-rest-site-list
+Description: Adds a rest endpoint for Wordpress multisite network installations to list all sites. Similar to wpcli `wp site list`.
+Version: 1.0.0
+Author: davidsword
+Author URI: https://davidsword.ca/
+License: GPLv3
+License URI: https://github.com/davidsword/network-rest-site-list/blob/master/LICENSE
+
+See README's
+See https://github.com/davidsword/network-rest-site-list
+*/
+
+defined("ABSPATH") || exit;
+
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'ntwrkrst/v1', '/wpsitelist', array(
+    'methods'  => WP_REST_Server::READABLE,
+    'callback' => function($request){
+        global $wpdb;
+
+        // only on the main site, it doesnt make sense to be on others
+        if ( get_current_blog_id() != 1 ) return;
+
+        // cache for 24 h to prevent unnessisary query
+        $trankey = "_network_rest_site_list";
+        if (false === ( $data = get_transient( $trankey ) ) ) {
+            $sites = $wpdb->get_results("SELECT * FROM `{$wpdb->prefix}blogs` ORDER BY `blog_id`");
+            $data = [];
+            foreach ($sites as $site) {
+                $data[$site->blog_id] = [
+                    'blog_id' => $site->blog_id,
+                    'domain' => $site->domain,
+                    'path' => $site->path,
+                ];
+            }
+            set_transient( $trankey, $data, 60*60*24 );
+        }
+
+        // if we're searching, filter with php (instead of mysql) to preserve cache
+        if (isset($_GET['q']) || isset($_GET['query'])) {
+            foreach ($data as $k => $site)
+                if (!strstr($site['path'],$_GET['q']))
+                    unset($data[$k]);
+        }
+
+        // return an error in the form of a result if nothing found
+        if (count($data) < 1) {
+            $noresults = 'no results found';
+            $data = ['blog_id' => 0, 'domain' => $noresults, 'path' => $noresults];
+        }
+
+        // that's all folks
+        return $data;
+    },
+  ) );
+} );
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Network Rest Site List ===
+Contributors: davidsword
+Tags: REST, API, WP JSON, NETWORK, MULTISITE, RESTFUL
+Requires at least: 4.5
+Tested up to: 4.9.2
+Stable tag: trunk
+License: GPLv3
+License URI: https://github.com/davidsword/network-rest-site-list/blob/master/LICENSE
+
+Creates a REST endpoint to list all sites and their IDs in a Wordpress Multisite Network
+
+== Description ==
+
+🚀 Access the endpoint with /wp-json/ntwrkrst/v1/wpsitelist
+🔬 Query sites path with a search, similar to /wp-json/ntwrkrst/v1/wpsitelist?q=keyword
+📦 Cache's results for 24h
+⚙️ Built primarily for an Alfred workflow, available here: (@TODO link)
+
+== Installation ==
+
+= From the WordPress plugin directory (recommended): =
+
+1. Navigate to Plugins > Add New in your WordPress Dashboard
+1. Search for "Network Rest Site List"
+1. Click Install on the "Network Rest Site List" plugin
+1. Activate the plugin
+
+= By direct upload: =
+
+1. Download the plugin and unzip it.
+1. Upload the network-rest-site-list folder to your /wp-content/plugins/ directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.0.0 =
+* 20180508
+* Initial public implementation
